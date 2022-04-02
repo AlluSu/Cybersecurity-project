cf3ac026f044390c4413d754cc9bffc400b74df6
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2022-04-02 11:23:27 +0300

    Added missing datatype for schema which caused error in heroku psql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
-CREATE TABLE notes (id SERIAL PRIMARY KEY, content TEXT, removed BOOLEAN, user_id REFERENCES users);
+CREATE TABLE notes (id SERIAL PRIMARY KEY, content TEXT, removed BOOLEAN, user_id INTEGER REFERENCES users);
